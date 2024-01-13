8f37d1d8ed3ebb1625f439414d9367a1c07d31f0
jmvolkman <155846563+jmvolkman@users.noreply.github.com>
2024-01-13 16:05:50 +0000

    commit

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -24,6 +24,7 @@ fn main() {
 
     let tup: (i32, f64, u8) = (500, 6.4, 1);
     let (x, y, _z) = tup;
+    
     println!("The value of x is: {}", x);
     println!("The value of **y** is: **{}**", y);
     
