65510ad839a1a35b5a50c6b44d0938a686f81204
jmvolkman <155846563+jmvolkman@users.noreply.github.com>
2024-01-13 14:26:48 +0000

    c

--- tests/test_main.rs
+++ tests/test_main.rs
@@ -1,1 +1,31 @@
 // update this file with your own tests
+fn main() {
+    println!("Hello, world!");
+
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("The value of THREE_HOURS_IN_SECONDS is: {}", THREE_HOURS_IN_SECONDS);
+
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is: {}", x);
+
+    let spaces = "   ";
+    
+    let spaces = spaces.len();
+    println!("The value of spaces is: {}", spaces);
+
+    let guess: u32 = "42".parse().expect("Not a number!");
+    println!("The value of guess is: {}", guess);
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, _z) = tup;
+    println!("The value of x is: {}", x);
+    println!("The value of **y** is: **{}**", y);
+    
+}
